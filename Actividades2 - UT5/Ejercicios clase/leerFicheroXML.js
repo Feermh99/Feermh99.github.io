e1a4b5e8e8f229c27329a6dbff82b680f8afc11e
Feermh99 <76481386+Feermh99@users.noreply.github.com>
2021-05-20 10:17:19 +0200

    Update leerFicheroXML.js

--- Actividades2 - UT5/Ejercicios clase/leerFicheroXML.js
+++ Actividades2 - UT5/Ejercicios clase/leerFicheroXML.js
@@ -2,9 +2,12 @@ function gestionarFicheroXML(xmlDoc){
 	/*Recuperar la capa vacia*/
 	let capaVacia = document.querySelector("#ficheroXML")
 	let libros = xmlDoc.querySelectorAll("libro")
-	for(let i=0; i<libros.length; i++)
+	for(let i=0; i<libros.length; i++){
+		if(i == 3){
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='amarillo'>" + libros[i].textContent + "</p>"
+		}
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + libros[i].textContent + "</p>"
-	
+	}
 }
 
 loadDocA("libros.xml","xml");
